46b2dd64eae7b44aeadf2319f8f9fd874e0a781b
Colin Murtaugh <cmurtaugh@gmail.com>
2019-04-04 13:01:24 -0400

    added setup.py and other package parts

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ssm-parameter-store',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple python module to load secure settings from AWS SSM Parameter Store.',
+    long_description=README,
+    url='https://huit.harvard.edu/',
+    author='HUIT Academic Technology',
+    author_email='academictechnology@harvard.edu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        "boto3>=1.9.91",
+        "PyYAML>=3.13",
+    ],
+)
