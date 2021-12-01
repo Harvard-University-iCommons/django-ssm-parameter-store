2590ce40211429e5ffae0085182482c6c143db49
Yates, Elliott <elliott_yates@harvard.edu>
2021-12-01 13:51:17 -0500

    TLT-4090 (chore) bump version to 0.5, support python 3.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ssm-parameter-store',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
